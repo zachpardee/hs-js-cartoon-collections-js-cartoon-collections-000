f4e4c9fe4269911a9c8e66fe20c9bd3f1001931d
awesome-edition-6075 <zachpardee@gmail.com>
2019-01-29 22:08:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
 function dwarfRollCall(dwarves) {
+  var dwarvesRollCall = "";
+  
+  for(var i = 0; i<dwarves.length; i++)
+  {
+    dwarvesRollCall = dwarvesRollCall + (i+1) + ". " + dwarves[i] + " ";
+  }
+  
+  return dwarvesRollCall;
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var exclaimedArray = [];
+  
+  for (var i = 0; i<planeteerCalls.length; i++)
+  {
+  exclaimedArray.push(planeteerCalls[i].toUpperCase() + "!")
+  }
+  
+  return exclaimedArray;
 }
 
 function longPlaneteerCalls(words) {
+  for (var i = 0; i<words.length; i++)
+  {
+    var charCount = words[i].length;
+    if (charCount > 4)
+    {
+      return true
+    }
+    return false
+  }
 }
 
 function findTheCheese (foods) {
+  for (var i = 0; i<foods.length; i++)
+  {
+    if (foods[i] === "cheddar" || foods[i] === "gouda" || foods[i] === "camembert")
+    {
+      return foods[i];
+    }
+  }
+  return "no cheese!";
 }
